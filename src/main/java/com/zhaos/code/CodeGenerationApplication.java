c3ed651ecb1903d9f20cd542612b558b36835cab
jiesi <jiesi@dtstack.com>
2021-08-03 16:21:28 +0800

    Initial commit

---
+++ src/main/java/com/zhaos/code/CodeGenerationApplication.java
@@ -0,0 +1,18 @@
+package com.zhaos.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 各写一个demo
+ * mybatis-plus的代码生成器
+ * easyCode代码生成器
+ */
+@SpringBootApplication
+public class CodeGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGenerationApplication.class, args);
+    }
+
+}
